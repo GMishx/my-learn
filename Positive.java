17200f2ac5ff6e8af38b713cc59af065d7506f36
gaurav mishra <gmishx@gmail.com>
2017-04-14 14:03:35 +0530

    First

---
+++ Positive.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Positive{
+	public static void main(){
+		String input;
+		Scanner sc=new Scanner(System.in);
+		input = sc.next();
+		int in = Integer.parseInt(input);
+		if(in>=1){
+			System.out.println(35);
+		}
+		else if(n==0){
+			System.out.println(0);
+		}
+		else{
+			System.out.println(-1);
+		}
+	}
+}
